565329bacdda452134fef9ee89dda6e164e399b3
Md Saiful Islam <cse.saiful2119@gmail.com>
2024-10-18 23:45:00 +0600

    Time: 9 ms (67.71%), Space: 41.1 MB (61.11%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -4,27 +4,21 @@ class Solution {
         for(int n:nums){
             maxORValue = maxORValue | n;
         }
-        HashMap<HashMap<Integer, Integer>, Integer> mem = new HashMap<>();
+        
         int subset = 0;
         for(int i=0;i<nums.length;i++){
-            subset+=calculate(nums, i+1, nums[i], maxORValue, mem);
+            subset+=calculate(nums, i+1, nums[i], maxORValue);
         }
         return subset;
     }
     
     
-    public int calculate(int[] nums, int index, int orValue, int target,HashMap<HashMap<Integer, Integer>, Integer> mem  ){
+    public int calculate(int[] nums, int index, int orValue, int target){
         if(index==nums.length){
             if(orValue==target) return 1;
             else return 0;
         }
-        
-        HashMap<Integer, Integer> key = new HashMap<>();
-        key.put(index, orValue);
-        if(mem.get(key)!=null) return mem.get(key);
-        
-        
-        mem.put(key, calculate(nums, index+1,orValue, target,mem) + calculate(nums, index+1, orValue | nums[index], target,mem));
-        return mem.get(key);
+
+        return calculate(nums, index+1,orValue, target) + calculate(nums, index+1, orValue | nums[index], target);
     }
 }
